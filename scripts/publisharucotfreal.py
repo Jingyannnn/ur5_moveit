07ee6be624c882f415005cfa7979e7592b13d57d
jingyan <wujingyan783@gmail.com>
2023-03-07 14:16:26 +0700

    error premature end of jpeg, use imwrite and imread

--- scripts/publisharucotfreal.py
+++ scripts/publisharucotfreal.py
@@ -60,14 +60,14 @@ def calibratecamera():
 
 def publishtf(cap, mtx, dist):
     
-    ret, frame = cap.read()
+    # ret, frame = cap
     #if ret returns false, there is likely a problem with the webcam/camera.
     #In that case uncomment the below line, which will replace the empty frame 
     #with a test image used in the opencv docs for aruco at https://www.docs.opencv.org/4.5.3/singlemarkersoriginal.jpg
     # frame = cv2.imread('./images/test image.jpg') 
 
     # operations on the frame
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(cap, cv2.COLOR_BGR2GRAY)
 
     # set dictionary size depending on the aruco marker selected
     aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
@@ -81,7 +81,6 @@ def publishtf(cap, mtx, dist):
 
     # font for displaying text (below)
     font = cv2.FONT_HERSHEY_SIMPLEX
-    print ("123")
 
     # check if the ids list is not empty
     # if no check is added the code will crash
@@ -98,10 +97,10 @@ def publishtf(cap, mtx, dist):
 
         for i in range(0, ids.size):
             # draw axis for the aruco markers
-            cv2.drawFrameAxes(frame, mtx, dist, rvec[i], tvec[i], 0.1)
+            cv2.drawFrameAxes(cap, mtx, dist, rvec[i], tvec[i], 0.1)
 
         # draw a square around the markers
-        aruco.drawDetectedMarkers(frame, corners)
+        aruco.drawDetectedMarkers(cap, corners)
 
 
         # code to show ids of the marker found
@@ -109,15 +108,15 @@ def publishtf(cap, mtx, dist):
         for i in range(0, ids.size):
             strg += str(ids[i][0])+', '
 
-        cv2.putText(frame, "Id: " + strg, (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
+        cv2.putText(cap, "Id: " + strg, (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
 
 
     else:
         # code to show 'No Ids' when no markers are found
-        cv2.putText(frame, "No Ids", (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
+        cv2.putText(cap, "No Ids", (0,64), font, 1, (0,255,0),2,cv2.LINE_AA)
 
     # display the resulting frame
-    cv2.imshow('frame',frame)
+    cv2.imshow('frame',cap)
     # if cv2.waitKey(1) & 0xFF == ord('q'):
     #     break
 
@@ -137,7 +136,7 @@ def publishtf(cap, mtx, dist):
 class ImageConvert:
 
     def __init__(self):
-
+        self.node_name="cv_bridge_demo"
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("ur5/camera1/image_raw",Image,self.callback)
 
@@ -146,7 +145,9 @@ class ImageConvert:
             self.cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
-        
+        else:
+            self.img = cv2.imwrite('camera_image.jpeg', self.cv_image)
+            
 
         # (rows,cols,channels) = cv_image.shapeargs[
         # if cols > 60 and rows > 60 :
@@ -162,13 +163,13 @@ class ImageConvert:
 
     def get_data(self):
 
-        return self.cv_image
+        return self.img
 
 # def talker(quaternion, xyz,rvec,tvec):
     
 if __name__ == '__main__':
     try:
-        # image_convert = ImageConvert()
+        image_convert = ImageConvert()
         pub = rospy.Publisher('arucotf', Pose, queue_size=10)
         rospy.init_node('talker', anonymous=True)
         rate = rospy.Rate(10) # 10hz
@@ -176,8 +177,9 @@ if __name__ == '__main__':
         
 
         while not rospy.is_shutdown():
-            image_convert = ImageConvert()
-            cap=cv2.imread(image_convert.get_data)
+            # cap=image_convert.get_data()
+            temp_cap=cv2.imread(cv2.samples.findFile("camera_image.jpeg"))
+            cap=np.array(temp_cap, dtype=np.uint8)
             quaternion,tvec = publishtf(cap, mtx, dist)
             # Pose pose
             pose = Pose()
