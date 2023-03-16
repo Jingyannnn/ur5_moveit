f65b65ccb4a5a8e51cf416d30c34cc94ae418362
jingyan <wujingyan783@gmail.com>
2023-03-16 12:56:55 +0700

    get position

--- scripts/getposition.cpp
+++ scripts/getposition.cpp
@@ -1,10 +1,17 @@
-# This script is to transfer aruco/camera pose to 4*4 matrix and use tf listener to call tf camera/baselink
-include "ros/ros.h"
-include "std_msgs/String.h"
+// This script is to transfer aruco/camera pose to 4*4 matrix and use tf listener to call tf camera/baselink
+#include "ros/ros.h"
+#include "std_msgs/String.h"
 
 void Callback(const std_msgs::String::ConstPtr& msg)
 {
   ROS_INFO("I heard: [%s]", msg->data.c_str());
+
+  Transform() {}
+  explicit TF2SIMD_FORCE_INLINE Transform(const Quaternion& q, 
+        const Vector3& c = Vector3(tf2Scalar(0), tf2Scalar(0), tf2Scalar(0))) 
+        : m_basis(q),
+        m_origin(c)
+  {}
 }
 
 int main(int argc, char **argv)
